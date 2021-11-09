553eb34f9f9feb1b5c92040bd390197765a668b1
Max_Kavlanov <maxkavlanov110@gmail.com>
2021-11-09 12:49:09 +0500

    homework-3

---
+++ home3.0.py
@@ -0,0 +1,7 @@
+i=2
+num=int(input('enter prime number! '))
+x=2
+while num%2!=0:
+    while x<num:
+        print(x)
+        x+=1
---
+++ home3.py
@@ -0,0 +1,38 @@
+# a=2
+# num=int(input())
+# while num > a :
+#   if num%a==0 :
+#     print('True')
+#     a=a+1
+#   else:
+#     print('False')
+#     a=(num)+1
+
+
+# i=2
+# num=int(input())
+# x=2
+# while num%2==0:
+#     while x<num:
+#         print(x)
+#         x+=2
+# 
+# from turtle import *
+# color('green')
+# bgcolor('black')
+# speed(15)
+# hideturtle()
+# b=0
+# while b<200:
+#     right(b)
+#     forward(b*2)
+#     b=b+1
+# covid_19
+
+# square=float(input('введите сторону квадрата: '))
+# s=square**2
+# p=square*4
+# print(s,p)
+
+
+   
